eec29ee84d453b8ba7b2e5af687b382fbefb29fb
Tengfei Yin <tengfei.yin@sbgenomics.com>
2015-08-17 13:08:45 -0400

    initial checkin for testing

--- tests/testthat/test-easy-Auth.R
+++ tests/testthat/test-easy-Auth.R
@@ -2,4 +2,13 @@ library(sbgr)
 
 context("Auth Class")
 
-expect_is(Auth(token), "Auth")
+## check_token <- function() {
+##   if (missing(token)) {
+##     skip("token not available")
+##   }
+## }
+
+test_that("Test Auth constructor",{
+    expect_is(Auth("fake_token"), "Auth")
+    expect_error(Auth(token))
+})
